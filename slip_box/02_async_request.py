f1c974668c6ff03af6c12d29988930346ed954b5
MarcinDudek <dudek.v@gmail.com>
2025-05-22 18:15:53 +0200

    refactor: organize model definitions into Enum for clarity

--- slip_box/02_async_request.py
+++ slip_box/02_async_request.py
@@ -1,7 +1,8 @@
 from google import genai
 from dotenv import load_dotenv
-import asyncio
+from enum import Enum
 
+import asyncio
 import os
 
 load_dotenv()
@@ -10,15 +11,17 @@ API_KEY = os.getenv("API_KEY")
 
 
 client = genai.Client(api_key=API_KEY)
-model = "gemini-2.0-flash",
-model = "gemini-2.5-flash-preview-05-20"
+
+class ModelName(Enum):
+    GEMINI_2_0_FLASH = "gemini-2.0-flash",
+    GEMINI_2_5_FLASH_PREVIEW = "gemini-2.5-flash-preview-05-20"
 
 
 async def main():
 
     response = await client.aio.models.generate_content(
-        model=model,
-        contents="make summary of this video: https://youtu.be/bwz3Z9GXLyI?si=-pr157wnyggKjwxL"
+        model = ModelName.GEMINI_2_5_FLASH_PREVIEW,
+        contents = "make summary of this video: https://youtu.be/bwz3Z9GXLyI?si=-pr157wnyggKjwxL"
     )
 
     print(response.text)
